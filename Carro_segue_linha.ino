0c4644f2918dd569b7776f11d7be867351a8b580
EliederSousa <eliedersousa@gmail.com>
2020-11-19 00:16:29 -0300

    Update Carro_segue_linha.ino

--- Carro_segue_linha.ino
+++ Carro_segue_linha.ino
@@ -11,16 +11,15 @@ int pin_velocidadeD     = 10;
 int pin_velocidadeE     = 11;
 int pin_sensorLinhaD    = 12;
 
-
 #define FRENTE  true
 #define TRAS    false
 #define NALINHA true
-#define VELOCIDADE 80
+#define VELOCIDADE 200
 #define DELAY 0
 
 // Constantes usadas na lógica do sensor ultrassônico
 #define MAX_DISTANCE 300 // Distância máxima que vamos verificar no sensor ultrassônico (em centímetros). O máximo é 400-500cm.
-#define DIST_PARADA 20 // Distância mínima antes do carrinho parar para evitar colisões (em centimetros).
+#define DIST_PARADA 10 // Distância mínima antes do carrinho parar para evitar colisões (em centimetros).
 #define TEMPO_ESPERA 1500 // Por quanto tempo o carrinho esperará após ter parado para evitar uma colisão (em milésimos).
 
 // Configura a biblioteca NewPing, os pinos e a distância máxima.
@@ -29,11 +28,9 @@ NewPing sonar(pin_sonarTRIG, pin_sonarECHO, MAX_DISTANCE);
 void girarRodaDireita( bool sentido ) {
   analogWrite( pin_velocidadeD, VELOCIDADE );
   if ( sentido ) {
-    //saida( "RodaDireita( FRENTE )", 0 );
     digitalWrite(pin_mtDireito1, LOW);
     digitalWrite(pin_mtDireito2, HIGH);
   } else {
-    //saida( "RodaDireita( TRAS )", 0 );
     digitalWrite(pin_mtDireito1, HIGH);
     digitalWrite(pin_mtDireito2, LOW);
   }
@@ -42,40 +39,26 @@ void girarRodaDireita( bool sentido ) {
 void girarRodaEsquerda( bool sentido ) {
   analogWrite( pin_velocidadeE, VELOCIDADE );
   if ( sentido ) {
-    //saida( "RodaEsquerda( FRENTE )", 0 );
     digitalWrite(pin_mtEsquerdo1, LOW);
     digitalWrite(pin_mtEsquerdo2, HIGH);
   } else {
-    //saida( "RodaEsquerda( TRAS )", 0 );
     digitalWrite(pin_mtEsquerdo1, HIGH);
     digitalWrite(pin_mtEsquerdo2, LOW);
   }
 }
 
-void pararRodaDireita() {
-  digitalWrite(pin_mtDireito1, LOW);
-  digitalWrite(pin_mtDireito2, LOW);
-}
-
-void pararRodaEsquerda() {
-  digitalWrite(pin_mtEsquerdo1, LOW);
-  digitalWrite(pin_mtEsquerdo2, LOW);
-}
-
 void andar() {
-  //saida( "Andar()", 0 );
   girarRodaDireita( FRENTE );
   girarRodaEsquerda( FRENTE );
-  delay(50);
+  delay(30);
 }
 
 void parar() {
-  //saida( "Parar()", 0 );
   digitalWrite(pin_mtDireito1, LOW);
   digitalWrite(pin_mtDireito2, LOW);
   digitalWrite(pin_mtEsquerdo1, LOW);
   digitalWrite(pin_mtEsquerdo2, LOW);
-  delay(10);
+  delay(5);
 }
 
 void setup() {
@@ -93,20 +76,21 @@ void setup() {
   Serial.begin(9600);
 }
 
+// Função para debug do código. 
 void saida( const char* mensagem, int valor ) {
   Serial.print( mensagem );
   Serial.println( valor );
 }
 
 void loop() {
-
-  // Verifica o estado dos sensores.
+  
+  // Lemos as informações dos sensores.
   int distancia = sonar.ping_cm();
   bool sensorDireito = digitalRead( pin_sensorLinhaD );
   bool sensorEsquerdo = digitalRead( pin_sensorLinhaE );
-
+  
   // Para o carrinho se encontrar um obstáculo na frente.
-  while ( (distancia <= DIST_PARADA) && (distancia != 0) ) {
+  while ( (distancia <= DIST_PARADA) && (distancia > 0) ) {
     parar();
     delay( TEMPO_ESPERA );
     distancia = sonar.ping_cm();
@@ -127,4 +111,5 @@ void loop() {
     andar();
     parar();
   }
+  delay( 20 );
 }
